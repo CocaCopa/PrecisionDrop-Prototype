a8de8a2132310df1cda992b34add56f07b3db11b
John Pandis <ioanpand@outlook.com>
2026-01-28 14:26:53 +0200

    Update UnityInput.cs
    Fixed a bug where the mouse drag value wasn't read properly

--- Assets/_Scripts/PrecisionDrop/Input/Unity/UnityInput.cs
+++ Assets/_Scripts/PrecisionDrop/Input/Unity/UnityInput.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace PrecisionDrop.Input.Unity {
-    public sealed class UnityInput : MonoBehaviour, IInputSource {
+    internal sealed class UnityInput : MonoBehaviour, IInputSource {
+        [SerializeField, Min(0f)] private float mouseSensitivity;
+
         public bool IsHolding { get; private set; }
-        public Vector2 MouseDragDelta { get; private set; }
+        public Vector2 MouseDragDelta => IsHolding ? playerActions.Player.LeftMouseDrag.ReadValue<Vector2>() : Vector2.zero;
 
         private PlayerInputActions playerActions;
 
@@ -18,13 +20,9 @@ namespace PrecisionDrop.Input.Unity {
 
             playerActions.Player.ClickHold.performed += OnPress;
             playerActions.Player.ClickHold.canceled += OnRelease;
-
-            playerActions.Player.LeftMouseDrag.performed += OnDelta;
         }
 
         private void OnDisable() {
-            playerActions.Player.LeftMouseDrag.performed -= OnDelta;
-
             playerActions.Player.ClickHold.performed -= OnPress;
             playerActions.Player.ClickHold.canceled -= OnRelease;
 
@@ -33,27 +31,15 @@ namespace PrecisionDrop.Input.Unity {
 
         public Vector2 ConsumeDragDelta() {
             var d = MouseDragDelta;
-            MouseDragDelta = Vector2.zero;
             return d;
         }
 
         private void OnPress(InputAction.CallbackContext _) {
             IsHolding = true;
-            MouseDragDelta = Vector2.zero;
         }
 
         private void OnRelease(InputAction.CallbackContext _) {
             IsHolding = false;
-            MouseDragDelta = Vector2.zero;
-        }
-
-        private void OnDelta(InputAction.CallbackContext ctx) {
-            if (!IsHolding) return;
-
-            var d = ctx.ReadValue<Vector2>();
-            if (d.sqrMagnitude < 0.0001f) return;
-
-            MouseDragDelta += d;
         }
     }
 }
