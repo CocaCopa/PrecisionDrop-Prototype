260c90fd490e34488e314ee8d79079d17b86ba41
John Pandis <ioanpand@outlook.com>
2026-02-07 03:19:11 +0200

    Integrate level themes into AppBootstrapper
    Add theme selection and wiring to the app bootstrapper: introduce a ThemeSelectorAsset field, select the LevelThemeAsset at Awake, and construct PlayerTheme and PlatformTheme helpers to pass into playerSystem and platformsSystem.
    
    Update system installation order and signatures: gameFlowSystem.Install now receives the player API and event bus, playerSystem.Install accepts a PlayerTheme, and platformsSystem.Install accepts a PlatformTheme.
    
    Add null-check validation (throwing NullReferenceException) for themeSelectorAsset and playerSystem and remove the old OnValidate checks. Also initialize platformsSystem in Start.

--- Assets/_Scripts/PrecisionDrop/App/Unity/AppBootstrapper.cs
+++ Assets/_Scripts/PrecisionDrop/App/Unity/AppBootstrapper.cs
@@ -2,39 +2,53 @@ using System;
 using PrecisionDrop.GameFlow.Unity;
 using PrecisionDrop.LevelManagment.Unity;
 using PrecisionDrop.Platforms.Unity;
+using PrecisionDrop.Platforms.Unity.Presentation;
 using PrecisionDrop.Player.Unity;
+using PrecisionDrop.Player.Unity.Presentation;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace PrecisionDrop.App.Unity {
     public sealed class AppBootstrapper : MonoBehaviour {
+        [SerializeField] private ThemeSelectorAsset themeSelectorAsset;
+        [Space(10f)]
         [SerializeField] private PlayerSystem playerSystem;
         [SerializeField] private GameFlowSystem gameFlowSystem;
         [SerializeField] private LevelGeneratorSystem levelGeneratorSystem;
         [SerializeField] private PlatformsSystem platformsSystem;
 
-        private void OnValidate() {
-            if (!platformsSystem) Debug.LogError(Msg(nameof(platformsSystem)), this);
-            if (!levelGeneratorSystem) Debug.LogError(Msg(nameof(levelGeneratorSystem)), this);
-            if (!gameFlowSystem) Debug.LogError(Msg(nameof(gameFlowSystem)), this);
-            if (!platformsSystem) Debug.LogError(Msg(nameof(platformsSystem)), this);
-        }
-
         private void Awake() {
             ValidateSceneWiring();
-
-            gameFlowSystem.Install(platformsSystem.EventBus);
+            var levelTheme = themeSelectorAsset.Select();
+            
+            gameFlowSystem.Install(playerSystem.PlayerApi, platformsSystem.EventBus);
             levelGeneratorSystem.Install(platformsSystem.Builder);
-            playerSystem.Install(gameFlowSystem.Api);
+            playerSystem.Install(gameFlowSystem.Api, CreatePlayerTheme(levelTheme));
+            platformsSystem.Install(CreatePlatformTheme(levelTheme));
         }
 
         private void Start() {
+            platformsSystem.Init();
             gameFlowSystem.Init();
             levelGeneratorSystem.Init();
             playerSystem.Init();
         }
 
+        private static PlayerTheme CreatePlayerTheme(LevelThemeAsset levelTheme) {
+            return new PlayerTheme(
+                levelTheme.PlayerMat
+            );
+        }
+
+        private static PlatformTheme CreatePlatformTheme(LevelThemeAsset levelTheme) {
+            return new PlatformTheme(
+                levelTheme.RegularPieceMat,
+                levelTheme.DangerPieceMat
+            );
+        }
+
         private void ValidateSceneWiring() {
+            if (!themeSelectorAsset) throw new NullReferenceException(Msg(nameof(themeSelectorAsset)));
+            if (!playerSystem) throw new NullReferenceException(Msg(nameof(playerSystem)));
             if (!platformsSystem) throw new NullReferenceException(Msg(nameof(platformsSystem)));
             if (!levelGeneratorSystem) throw new NullReferenceException(Msg(nameof(levelGeneratorSystem)));
             if (!gameFlowSystem) throw new NullReferenceException(Msg(nameof(gameFlowSystem)));
